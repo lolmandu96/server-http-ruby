6d0ddf06e5dde636026db51dd44ea9bb5b363436
lolmandu96 <98780788+lolmandu96@users.noreply.github.com>
2022-04-11 09:03:17 +0200

    Update start server.bat

--- start server.bat
+++ start server.bat
@@ -2,15 +2,23 @@
 
 set path=C:\SERVER
 
-
-
 REM mkdir C:\SERVER
 
 REM copy server\* C:\SERVER
 
-set /p port="choose your port (int): "
+echo (if port is empty, the port will be 8080)
+set /p port="Choose your port (int): "
 
-echo 0.0.0.0:%port%
+if (%port%) == () (
+set port=8080
+)
+
+
+
+
+
+
+echo hosted at http://0.0.0.0:%port%
 
 REM ruby -run -e httpd %path% -p %port%
 
